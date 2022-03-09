bd9bb97f4ed7a94822b3e07de1e24de7edc50792
ivykw <ivyw219@gmail.com>
2022-03-09 14:32:46 -0600

    starting

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import Search from './Search.jsx';
+import MovieList from './MovieList.jsx';
 
-const App = (props) => (
-  <div>Hello World!</div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      found: []
+    }
+  }
+  onSubmit() {
+    // clear list
+    // display all matches (or partial matches) to the title
+  }
+  render() {
+    return (
+      <div>
+        <h2>MovieList</h2>
+          <input id="search-bar"></input>
+          <button id="go">Go!</button>
+          <div id="movie-list"><MovieList movies={movies}/></div>
+      </div>
+    );
+  }
+}
+
+var movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
 
 export default App;
---
+++ client/src/components/MovieList.jsx
@@ -0,0 +1,11 @@
+var MovieList = (props) => (
+  <div>
+    <div>{props[0].title}</div>
+    <div>{props[1].title}</div>
+    <div>{props[2].title}</div>
+    <div>{props[3].title}</div>
+    <div>{props[4].title}</div>
+  </div>
+);
+
+export default MovieList;
